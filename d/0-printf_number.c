84b5ee4656be691ec51fbcf9907aa888ac8da8f7
William McCann <william.mccann@holbertonschool.com>
2016-02-13 23:52:31 +0000

    uploading zero

---
+++ d/0-printf_number.c
@@ -0,0 +1,13 @@
+/*header includes prototype for printf and
+standard library*/
+#include "my_functions.h"
+
+/*start of function; function take an int
+called "n" and returns void*/
+void printf_number(int n)
+{	
+/*printf is a function from the standard
+library that outputs printed text.  The
+%d is a format specifier for ints.*/
+	printf("%d",n);
+}	
