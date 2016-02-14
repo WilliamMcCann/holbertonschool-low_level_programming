10095a2b1d70d005e8ee6948a5040d39cd6975d0
Nathan McCann <45@p2-3.hbtn.io>
2016-02-13 19:46:18 -0800

    upload 1

--- dreams_and_dedication_are_a_powerful_combination/1-print_combination_1.c
+++ dreams_and_dedication_are_a_powerful_combination/1-print_combination_1.c
@@ -1,22 +1,27 @@
 /*header to include prototypes*/
 #include "my_functions.h"
 
-/*main function, takes "int n" as its
-input, gives no output*/
-void print_number(int n)
+/*main function, takes void and passes void*/
+void print_combination_1 (void)
 {
-/*prints the char*/
+/*declare variable "n"*/
+	int n;
+/*for loop initializes n at zero, cycles while
+n is less than ten, increments n by one*/
+	for(n = 0; n < 10; n++){
+/*prints char, uses the "add zero" trick to
+get proper digit and convert int to char*/
+		print_char('0' + n);
 
-
-int i=0,a;
-while(i<=9)
-{
-print_char("%d",(a%10));
-} 
-i++
-
-/*prints the new line*/
-  print_char('\n');
+/*if statement works with numbers less than nine,
+used to keep function from printing comma and
+space after the nine*/
+	if (n < 9)
+/*prints the commas*/
+		print_char(',');
+/*prints the spaces*/
+		print_char(' ');
+	}
 }
 
 
