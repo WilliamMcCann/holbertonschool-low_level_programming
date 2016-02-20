3fc141ca5dd2182ef2f213208145bf63b32714cd
Electra Chong <electrachong@gmail.com>
2016-02-20 14:28:01 -0800

    final upload

--- reefinery_C_0/main.c
+++ reefinery_C_0/main.c
@@ -7,6 +7,6 @@ int main(void)
   print_square(3);
   print_square(4);
   print_square(10);
-  print_square(-2);
+  print_square(0);
   return (0);
 }
--- reefinery_C_0/print_square.c
+++ reefinery_C_0/print_square.c
@@ -1,15 +1,15 @@
 /*include prototypes for print_char and others*/
-#include "reefinery_functions.h"
 int print_char(char c);
+void print_hyphens(int ncopy);
 
 /*function takes integer "n" and passes void*/
 void print_square(int n){
 
 	/*declares cleverly names variables*/
-	int hyphen, pipe, space, stack, ncopy;
+	int hyphen, space, stack, ncopy;
 
 	/*to preserve the value of "n" for later use*/
-	n = ncopy;
+	ncopy = n;
 
 	/*if n is less than zero*/
 	if (ncopy <= 0){
@@ -22,40 +22,34 @@ void print_square(int n){
  		print_char('o');
  		print_char('\n');
  	}
+	
+	if (ncopy >= 2){
+	  print_hyphens(ncopy);
 
-	if (ncopy == 2){
-		print_char('o');
-		print_char('o');
-		print_char('\n');
-		print_char('o');
-		print_char('o');
-		print_char('\n');
-	}	
+	  for (stack = ncopy -2 ; stack > 0 ; stack--){
+	     print_char('|');
+	     for (space = ncopy - 2 ; space > 0 ; space--){
+	        print_char(' ');
+	     }
+	     print_char('|');
+	     print_char('\n');
+	  }
+	  
+	  print_hyphens(ncopy);
 
-	if (ncopy > 2){
-		print_char('o');
-		for (hyphen = ncopy - 2 ; hyphen > 2 ; hyphen--){
-			print_char('-');
-		}
-		print_char('o');
-		print_char('\n');
-			for (stack = ncopy -2 ; stack > 2 ; stack--){
-				print_char('|');
-				for (space = ncopy - 2 ; space > 2 ; space--){
-				print_char(' ');
-				}
-				print_char('|');
-				print_char('\n');
-			}	
 	}
-	if (ncopy > 2){
-		print_char('o');
-		for (hyphen = ncopy - 2 ; hyphen > 2 ; hyphen--){
-			print_char('-');
-		}
-		print_char('o');
-		print_char('\n');
+}
 
-	}	
+void print_hyphens(int ncopy){
+
+  int hyphen;
+  
+  print_char('o');
+
+  for (hyphen = ncopy - 2 ; hyphen > 0 ; hyphen--){
+    print_char('-');
+ }
+  print_char('o');
+  print_char('\n');
 
 }
