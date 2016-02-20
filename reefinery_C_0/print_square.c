40fd08634dd8c89220782ff2d1e0fe8b677c2b75
William McCann <william.mccann@holbertonschool.com>
2016-02-20 20:04:44 +0000

    adding reef dir

---
+++ reefinery_C_0/print_square.c
@@ -0,0 +1,12 @@
+#include "reefinery_functions.h"
+int print_char(char c);
+
+void print_square(int n){
+
+	int hyphen, pipe, space, zero;
+
+	if (n == 0){
+		print_char('0');
+	}	
+	
+}
---
+++ reefinery_C_0/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+{
+	printf(":%s:\n", "o-o");
+	printf(":%s:\n", "| |");
+	printf(":%s:\n", "o-o");
+}
+return(0);
+	}
+
