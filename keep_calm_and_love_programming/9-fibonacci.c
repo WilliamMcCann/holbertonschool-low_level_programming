495ff1e2d82a2d740713dfa5d6a1964d80b0ab08
William McCann <william.mccann@holbertonschool.com>
2016-02-20 02:16:47 +0000

    adding 9

--- keep_calm_and_love_programming/9-fibonacci.c
+++ keep_calm_and_love_programming/9-fibonacci.c
@@ -1,46 +1,39 @@
-/*#include <stdio.h>
-
-int main(void)
-{
-   long long int f = 0, s = 1, next, c;
- 
-   
- 
-   for ( c = 0 ; c < 50 ; c++ )
-   {
-      if ( c <= 1 )
-         next = c;
-      else
-      {
-         next = f + s;
-         f = s;
-         s = next;
-      }
-      printf("%d\n",next);
-   }
- 
-   return 0;
-}
-*/
-
+/*header to include the prototype for printf*/
 #include <stdio.h>
  
-int main()
+ /*function takes nothing, passes an int*/
+int main(void)
 {
+	/*declares four long int variables,
+	initializes f to zero and s to one,
+	long int needed to handle the long
+	numbers at the end of the sequence*/
    long int f = 0, s = 1, n, c;
  
+ 	/*for loop initializing c at zero,
+ 	running while c is less than fifty,
+ 	and incrementing c by one*/
    for ( c = 0 ; c < 50 ; c++ )
-   {
+   {  
+   	  /*if c is less than or equal to one*/ 		
       if ( c <= 1 )
+      	/*make n equal to c*/
          n = c;
+      /*else statement runs most of the
+      function*/
       else
       {
+      	/*next int is made equal to first int
+      	plus second int*/
          n = f + s;
+         /*first in made equal to second int*/
          f = s;
+         /*second int made equal to next int*/
          s = n;
       }
+      /*prints int n*/
       printf("%li\n",n);
    }
- 
+ 	/*returns zero*/
    return 0;
 }
