a02614e4dedfeddf7ae4e7574a6980cc5c879cc4
William McCann <william.mccann@holbertonschool.com>
2016-02-20 00:18:24 +0000

    adding my_functions

---
+++ keep_calm_and_love_programming/my_functions.h
@@ -0,0 +1,7 @@
+int print_char(char c);
+void print_number(int n);
+void print_combination_1(void);
+void print_combination_2(void);
+void print_combination_3(void);
+void print_to_98(int);
+void print_combination_4(void);
