929e230383862524ca0f6e8ead8df1655d39076b
William McCann <william.mccann@holbertonschool.com>
2016-02-23 19:48:38 +0000

    adding 0

--- its_tuesday_dont_forget_to_be_awesome/0-first_digit.c
+++ its_tuesday_dont_forget_to_be_awesome/0-first_digit.c
@@ -1,1 +1,20 @@
-jfdkafjkahj;kfld;jkl
+/*include prototypes*/
+#include "my_functions.h"
+
+/*main function, takes an intger
+called "n", gives no output*/
+int first_digit(int n){
+
+	int length, number = n;
+
+    if (n > 0){
+		number = number * -1;    	
+        n = n * -1;      
+              } 
+
+    for (length = 0; number < -9; length++){
+    	number = number / 10;
+    	}
+
+	return(-number);
+}
