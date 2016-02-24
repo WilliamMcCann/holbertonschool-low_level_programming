f22c50474687fabf9adac072a75b8e8583a2a0f4
William McCann <william.mccann@holbertonschool.com>
2016-02-24 20:16:22 +0000

    commented 0

--- its_tuesday_dont_forget_to_be_awesome/0-first_digit.c
+++ its_tuesday_dont_forget_to_be_awesome/0-first_digit.c
@@ -5,16 +5,26 @@
 called "n", gives no output*/
 int first_digit(int n){
 
+	/*declares int variables length and number,
+	initializes number as n*/
 	int length, number = n;
-
+    /*for all numbers greater than zero*/	
     if (n > 0){
+		/*make number negative by multiplying
+		it by -1*/
 		number = number * -1;    	
+	/*make n negative by multiplying it by -1*/
         n = n * -1;      
               } 
-
+    /*for loop in which var length is initialized at
+    zero, loops while number is less than -9, then
+    increments length by one*/		
     for (length = 0; number < -9; length++){
+	/*number equals number divided by ten*/
     	number = number / 10;
     	}
-
+	/*return number, negative to balance out
+	when we swapped the positive numbers to negative
+	in the beginning*/
 	return(-number);
 }
