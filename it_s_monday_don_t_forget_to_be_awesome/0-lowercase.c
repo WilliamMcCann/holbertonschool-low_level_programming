cbc577c16c9589055e3d7d78b505171d30c84bd0
William McCann <william.mccann@holbertonschool.com>
2016-02-24 00:14:38 +0000

    reuploading 0

--- it_s_monday_don_t_forget_to_be_awesome/0-lowercase.c
+++ it_s_monday_don_t_forget_to_be_awesome/0-lowercase.c
@@ -4,13 +4,13 @@ char lowercase(char c){
 	/*if statement capture all ASCII chars
 	including and between "A" (ASCII 65) 
 	and "z" (ASCII 90)*/
-	if (c <= 122 && c >= 97){
+	if (c <= 90 && c >= 65){
 		/*subtracts 32 from the ASCII
 		value of the char, aligning the
 		lower-case alphabet with the
 		upper-case alphabet on the
 		ASCII chart*/	
-		(c = c - 32);
+		(c = c + 32);
 	}
 	/*return a char to the next program*/
 	return c;
