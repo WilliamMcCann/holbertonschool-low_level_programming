87d322e57ac915c3f9dce101f68e3ba110e76363
Nathan McCann <45@p7-4.hbtn.io>
2016-03-07 22:27:15 -0800

    adding 5

--- pointers_are_fun/5-string_to_integer.c
+++ pointers_are_fun/5-string_to_integer.c
@@ -1,37 +1,27 @@
-#include "my_functions.h"
-int string_to_integer(char *s);
+#include <limits.h>
 
-int string_to_integer(char *s){
-    
-    int i, m, p, n;
-    /*loops through string, stops at null*/
-    for (i=0; *(s+i) != '\0'; i++){
-        /*if statements evaluate for pos or neg*/
-        if (*(s+i) == 45) {
-            m++;
-        }
-        
-        
-        so per discussion w gus, use one big single loop.  if it detects a number after the initial number, it runs the loop *then* adds.  if not it breaks out of the loop (gotta do it this way so it can keep looping beyond the first two numbers)
-        
-        
-        
-        
-        /*picks out digits between zero and nine*/
-        if (*(s+i) >= 48 && *(s+i) <= 57){
-            /*n equals the digit in the string*/
-            n = *(s+i);
-            /*if the next char is a digit...*/
-            if (*(s+(i+1))) >= 48 && (*(s+(i+1))) <= 57){
-                /*...then multiply n by 10 and add the new digit*/
-                n = n * 10 + (*(s+(i+1)));
-        }
-        /*gives the number the proper sign*/    
-        if (m % 2 == 1){
-            (n * -1);
-        }
+int string_to_integer(char *s)
+{
+  long n = 0;
+  int m = 0, i, x = 0;
+
+  while (*(s+x) != '\0') {
+    if (*(s+x) == 45) {
+      m++;
     }
-        /*sends zero if no numbers present*/
-        else(n = 48);
-    return(n);
+    if (*(s+x) > 47 && *(s+x) < 58) {
+      i = (*(s+x) -48);
+      n = (n*10) + i;
+      if ((n > INT_MAX && m % 2 == 0) || (n < INT_MIN && m % 2 == 1)) {
+	return (0);
+      }
+      if (*(s+x+1) <= 47 || *(s+x+1) >= 58){
+	break;
+      }
+   }
+   x++;
+  }
+  if (m % 2 == 1) {
+    n = -n; }
+  return (n);
 }
