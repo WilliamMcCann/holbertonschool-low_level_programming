c9644348883687ecf2a232d6542df5c848b40b20
William McCann <william.mccann@holbertonschool.com>
2016-03-11 23:25:22 +0000

    adding four

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/3-string_ncopy.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/3-string_ncopy.c
@@ -1,1 +1,15 @@
 char *string_ncopy(char *dest, const char *src, int n);
+
+char *string_ncopy(char *dest, const char *src, int n)
+{
+    char *s = dest;
+    while (n > 0 && *src != '\0') {
+	*s++ = *src++;
+	--n;
+    }
+    while (n > 0) {
+	*s++ = '\0';
+	--n;
+    }
+    return dest;
+}
