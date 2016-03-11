612dd380980bfb03cebbd891321e0df5adb4d5da
William McCann <william.mccann@holbertonschool.com>
2016-03-11 22:00:06 +0000

    adding two

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/2-string_copy.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/2-string_copy.c
@@ -1,1 +1,9 @@
 char *string_copy(char *dest, const char *src);
+
+char *string_copy(char *dest, const char *src)
+{
+    char *s = dest;
+    while ((*s++ = *src++) != 0)
+	;
+    return (dest);
+}
