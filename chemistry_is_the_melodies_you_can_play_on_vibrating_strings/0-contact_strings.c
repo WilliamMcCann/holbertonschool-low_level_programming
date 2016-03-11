27e9fe79759008d9ccfa5c6f950c34f7ba9e6adc
William McCann <william.mccann@holbertonschool.com>
2016-03-11 20:42:12 +0000

    adding zero

---
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/0-contact_strings.c
@@ -0,0 +1,16 @@
+#include "my_functions.h"
+/*concatenates two strings*/
+char *concat_strings(char *dest, const char *src){
+ int i=0, j;
+
+ while (dest[i]!='\0'){
+   i++;
+}
+
+ for (j = 0; src[j] != '\0'; i++, j++) {
+   dest[i] = src[j];
+ }
+
+ /*dest[i] = '\0';*/
+ return dest ;
+}
