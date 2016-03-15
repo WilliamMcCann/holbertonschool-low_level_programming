5ddb1c3256b72202e61a6b0c0018353404c1b35c
William McCann <william.mccann@holbertonschool.com>
2016-03-15 22:02:18 +0000

    adding two

--- the_shortest_distance_between_two_points_is_a_straight_line/1-print_diagonal.c
+++ the_shortest_distance_between_two_points_is_a_straight_line/1-print_diagonal.c
@@ -1,1 +1,16 @@
-void print_diagonal(int n);
+int print_char(char c);
+
+void print_diagonal(int n){
+  int i, j;
+
+  if (n <= 0){
+    print_char('\n');
+  }
+  for ( i = 0; i < n ; i++ ) {
+      for ( j = 0 ; j < i ; j++) {
+        print_char(' ');
+        }
+        print_char('\\');
+        print_char('\n');
+  }
+}
