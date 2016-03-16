d6f5e4964b8ce5dfcbf2c5a583251282f4c0e365
William McCann <william.mccann@holbertonschool.com>
2016-03-16 22:12:25 +0000

    adding two

--- if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/2-1337.c
+++ if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/2-1337.c
@@ -1,1 +1,26 @@
-char *leet(char *);
+char *leet(char *z){
+int j;
+for(j=0; z[j] != '\0'; j++) {
+  if (z[j] == 'a')
+    z[j] = '4';
+  else if (z[j] == 'A')
+    z[j] = '4';
+  else if (z[j] == 'e')
+    z[j] = '3';
+  else if (z[j] == 'E')
+    z[j] = '3';
+  else if (z[j] == 'o')
+    z[j] = '0';
+  else if (z[j] == 'O')
+    z[j] = '0';
+  else if (z[j] == 't')
+    z[j] = '7';
+  else if (z[j] == 'T')
+    z[j] = '7';
+  else if (z[j] == 'l')
+      z[j] = '1';
+  else if (z[j] == 'L')
+      z[j] = '1';
+  }
+return(z);
+}
