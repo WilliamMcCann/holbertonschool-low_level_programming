454dd1adb73016165f989b909ee4189eeee9229d
William McCann <william.mccann@holbertonschool.com>
2016-04-29 22:40:05 +0000

    adding zero

---
+++ cisdoublefun_day_3_recursion/0-factorial.c
@@ -0,0 +1,22 @@
+#include <limits.h>
+
+int factorial(int n)
+{
+  int result;
+  int i;
+
+  result = 1;
+  i = 1;
+
+  if (n < 0 || n > 12)
+  {
+    return (-1);
+  }
+
+  while (i <= n)
+  {
+    result *= i;
+    i++;
+  }
+  return (result);
+}
