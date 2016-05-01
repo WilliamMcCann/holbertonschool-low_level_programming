a7e3f9d436fbfd52465d8d61bb28dd2057fdc09d
William McCann <william.mccann@holbertonschool.com>
2016-05-01 22:21:43 +0000

    readding three and four

--- cisdoublefun_day_5_libraries_argc_argv/3-args.c
+++ cisdoublefun_day_5_libraries_argc_argv/3-args.c
@@ -1,14 +1,27 @@
-#include<stdio.h>
+#include <unistd.h>
+
+int print_char(char c)
+{
+  return (write(1, &c, 1));
+}
 
 /*prints out all arguments passed via the command line*/
 void main (int argc, char **argv)
 {
   int i;
+  int j;
+  j = 0;
   i = 0;
 
   while (i < argc)
   {
-    printf("%s\n", argv[i]);
+    j = 0;
+    while (argv[i][j] != '\0')
+    {
+      print_char(argv[i][j]);
+      j++;
+    }
+    print_char('\n');
     i++;
   }
 }
--- cisdoublefun_day_5_libraries_argc_argv/4-args.c
+++ cisdoublefun_day_5_libraries_argc_argv/4-args.c
@@ -1,14 +1,31 @@
-#include<stdio.h>
+#include <unistd.h>
+
+int print_char(char c)
+{
+  return (write(1, &c, 1));
+}
 
 /*prints out all arguments passed via the command line*/
 void main (int argc, char **argv)
 {
   int i;
-  i = argc;
+  int j;
+  int k;
+  k = 0;
+  j = 0;
+  i = 0;
 
-  while (i > 0)
+  k = argc;
+  while (k > 0)
   {
-    printf("%s\n", argv[i - 1]);
+    j = 0;
+    while (argv[i][j] != '\0')
+    {
+      print_char(argv[i][j]);
+      j++;
+    }
+    print_char('\n');
+    k--;
     i--;
   }
 }
