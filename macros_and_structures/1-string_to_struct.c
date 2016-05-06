09bb11a89ff26ca8b9ea5914d39cbbc3963d1ad2
William McCann <william.mccann@holbertonschool.com>
2016-05-06 15:48:05 +0000

    adding first two again

--- macros_and_structures/1-string_to_struct.c
+++ macros_and_structures/1-string_to_struct.c
@@ -1,6 +1,7 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include "str_struct.h"
+char *string_copy(const char *src, int a);
 
 int str_len(char *str)
 {
@@ -14,17 +15,43 @@ int str_len(char *str)
   return (i);
 }
 
+char *string_copy(const char *src, int a)
+{
+  char *dest;
+  int i;
+  i = 0;
+
+  dest = malloc(sizeof(char) * a);
+
+  while (src[i] != '\0')
+  {
+      if (dest == NULL)
+    {
+      return (NULL);
+    }
+
+    dest[i] = src[i];
+    i++;
+  }
+  return (dest);
+}
+
 /*function that stores a string and its length in a newly allocated/
 structure, and return the address of this structure*/
-struct String *string_to_struct(char *var)
+struct String *string_to_struct(char *frommain)
 {
   struct String *str;
+  char *var;
+
+
+  var = string_copy(frommain, str_len(frommain));
 
   str = malloc(sizeof(struct String));
   if (str == NULL)
   {
     return (NULL);
   }
+
   str->str = var;
   str->length = str_len(var);
   return (str);
